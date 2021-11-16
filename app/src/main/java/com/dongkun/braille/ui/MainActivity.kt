60d9e00e7345c9910e36ab6e2b681e3af1aec85b
nicky8209 <nicky8209@naver.com>
2021-11-16 15:33:44 +0900

    Initial commit

--- app/src/main/java/com/dongkun/braille/ui/MainActivity.kt
+++ app/src/main/java/com/dongkun/braille/ui/MainActivity.kt
@@ -5,11 +5,13 @@ import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.os.Handler
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
@@ -18,27 +20,60 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.dongkun.braille.R
+import com.dongkun.braille.util.ACTIVITY_REQUEST_CODE
 import com.dongkun.braille.databinding.ActivityMainBinding
 import com.dongkun.braille.util.*
 import com.dongkun.braille.viewmodel.MainViewModel
+import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.*
 
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
-    internal var doubleBackToExitPressedOnce = false
+    private var doubleBackToExitPressedOnce = false
+    private var mBtnCameraView: Button? = null
+    private var mEditOcrResult: EditText? = null
 
-    val viewModel by viewModel<MainViewModel>()
+    private val viewModel by viewModel<MainViewModel>()
 
+    private var datapath = ""
+    private var lang = ""
     var mBluetoothAdapter: BluetoothAdapter? = null
     var recv: String = ""
+
+    var sTess: TessBaseAPI? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = viewModel
+
+
+        // 뷰 선언
+        mBtnCameraView = findViewById<Button>(R.id.btn_camera)
+        mBtnCameraView!!.setOnClickListener(mClickLister);
+        mEditOcrResult = findViewById<EditText>(R.id.txt_send)
+        sTess = TessBaseAPI()
+
+
+        // Tesseract 인식 언어를 한국어로 설정 및 초기화
+        lang = "kor"
+        datapath = "$filesDir/tesseract"
+
+        if (checkFile(File(datapath + "/tessdata"))) {
+            sTess!!.init(datapath, lang)
+        }
+
+
+        // Tesseract 인식 언어를 한국어로 설정 및 초기화
+
+
+        // Tesseract 인식 언어를 한국어로 설정 및 초기화
+        lang = "kor"
+        datapath = "$filesDir/tesseract"
 
         if (!hasPermissions(this, PERMISSIONS)) {
             requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
@@ -47,6 +82,69 @@ class MainActivity : AppCompatActivity() {
         initObserving()
 
     }
+
+    private fun checkFile(dir: File): Boolean {
+        //디렉토리가 없으면 디렉토리를 만들고 그 후에 파일을 카피
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles()
+        }
+        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+        if (dir.exists()) {
+            val datafilepath = "$datapath/tessdata/$lang.traineddata"
+            val datafile = File(datafilepath)
+            if (!datafile.exists()) {
+                copyFiles()
+            }
+        }
+        return true
+    }
+
+    private fun copyFiles() {
+        val assetMgr = this.assets
+        var `is`: InputStream? = null
+        var os: OutputStream? = null
+        try {
+            `is` = assetMgr.open("tessdata/$lang.traineddata")
+            val destFile = "$datapath/tessdata/$lang.traineddata"
+            os = FileOutputStream(destFile)
+            val buffer = ByteArray(1024)
+            var read: Int
+            while (`is`.read(buffer).also { read = it } != -1) {
+                os.write(buffer, 0, read)
+            }
+            `is`.close()
+            os.flush()
+            os.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == ACTIVITY_REQUEST_CODE) {
+                // 받아온 OCR 결과 출력
+                mEditOcrResult!!.setText(data!!.getStringExtra("STRING_OCR_RESULT"))
+            }
+        }
+    }
+
+    private val mClickLister: View.OnClickListener = object : View.OnClickListener {
+        override fun onClick(v: View?) {
+
+            // 버튼 클릭 시
+
+            // Camera 화면 띄우기
+            val mIttCamera = Intent(this@MainActivity, CameraView::class.java)
+            startActivityForResult(mIttCamera, ACTIVITY_REQUEST_CODE)
+
+
+        }
+    }
+
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
--- app/src/main/java/com/dongkun/braille/util/Constants.kt
+++ app/src/main/java/com/dongkun/braille/util/Constants.kt
@@ -3,8 +3,11 @@ package com.dongkun.braille.util
 import android.Manifest
 
 const val REQUEST_ALL_PERMISSION = 1
+const val ACTIVITY_REQUEST_CODE = 1
+const val PERMISSION_REQUEST_CODE = 1
 const val SPP_UUID = "00001101-0000-1000-8000-00805f9b34fb"
 const val isDebug = false
 val PERMISSIONS = arrayOf(
-    Manifest.permission.ACCESS_FINE_LOCATION
+    Manifest.permission.ACCESS_FINE_LOCATION,
+    Manifest.permission.CAMERA
 )
